4fd0993d4f85bd4c177b4ea1344a6b9cb04a5cfb
Tri Toan <nguyentritoanst@gmail.com>
2020-10-01 09:35:07 +0700

    add postgresql 13

--- build-vi.sh
+++ build-vi.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 DOCKER_NAME=tritoanst/postgresql
-docker pull bitnami/postgresql:12-debian-10
-docker build -f ./12/debian-10/Dockerfile -t "$DOCKER_NAME:12-vi" .
+docker pull bitnami/postgresql:13-debian-10
+docker build -f ./13/debian-10/Dockerfile -t "$DOCKER_NAME:13-vi" .
